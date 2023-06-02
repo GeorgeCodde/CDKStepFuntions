cedb7faf3ae674efcbab4a739555a785c93c2d12
JORGE LOPEZ SANCHEZ <jorgels120878@gmail.com>
2023-06-01 21:15:16 -0600

    First commit

--- lib/cdk_step_funtion-stack.ts
+++ lib/cdk_step_funtion-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import * as logs from 'aws-cdk-lib/aws-logs';
+
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +10,50 @@ export class CdkStepFuntionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const startState = new stepfunctions.Pass(this, 'PassState', {
+      result: stepfunctions.Result.fromObject({ Value: 'Pass' }),
+      resultPath: '$.result',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStepFuntionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // const waitState = new stepfunctions.Wait(this, 'WaitState', {
+    //   time: stepfunctions.WaitTime.secondsPath('$.triggerTime'),
+    // })
+    
+    const successState = new stepfunctions.Pass(this, 'SuccessState', {
+      result: stepfunctions.Result.fromObject({ Value: 'SuccessState' }),
+      resultPath: '$.result',
+    });
+    const failureState = new stepfunctions.Pass(this, 'FailureState', {
+      result: stepfunctions.Result.fromObject({ Value: 'FailureState' }),
+      resultPath: '$.result',
+    });
+
+    const choice = new stepfunctions.Choice(this, 'Did it work?', {
+      inputPath: '$.body',
+    });
+    const condi1 = stepfunctions.Condition.stringEquals('$.status', 'SUCCESS')
+    const cond2 = stepfunctions.Condition.stringEquals('$.status', 'FAIL')
+
+
+    const definitionState = startState.next(choice
+      .when(condi1, successState)
+      .when(cond2, failureState))
+    
+    /*.next(waitState);*/
+    
+    const logGroup = new logs.LogGroup(this, 'MyLogGroup');
+
+    const stateMachine = new stepfunctions.StateMachine(this, 'CDKStateMachine', {
+      definition: definitionState,
+      stateMachineType: stepfunctions.StateMachineType.EXPRESS,
+      logs: {
+        destination: logGroup,
+        level: stepfunctions.LogLevel.ALL,
+      },
+    })
+
+    const api = new apigateway.StepFunctionsRestApi(this, 'CDKStepFunctionsRestApi', {
+      stateMachine: stateMachine
+    });    
   }
 }
